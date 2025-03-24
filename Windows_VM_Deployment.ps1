2946c368edb4125848669e26602d6c9815ddc2fa
Static <118852722+jacenhyde@users.noreply.github.com>
2025-03-24 13:39:36 -0500

    Update Windows_VM_DeploymentGen1_Deployment.ps1

--- Windows_VM_Deployment.ps1
+++ Windows_VM_Deployment.ps1
@@ -1,14 +1,21 @@
 # T/S Notes
 # If you are getting an error about NTCreateFile, Make sure vboxsup is enabled via `sc.exe query vboxsup` if its STOPPED, then run `sc.exe start vboxsup`
 
-# Questions
-$VMName = Read-Host "What is the name of the VM?"
-$VMCPUs = Read-Host "How many CPU's would you like to provide?"
-$VMMemory = Read-Host "Enter the amount of RAM in MB, 8000MB = 8GB [Min reccomended for Windows10 is 8GB]"
-$VMMemSize = $VMMemory * 1024
-$VMDisk = Read-Host "Enter the disk size in GB [Min reccomended for Windows10 is 32GB]"
-$VMPath = "$env:USERPROFILE\VirtualBox VMs\$VMName"
+# Current Stage of Script: The Script runs successfully but does not autocreate a user or configure installation, all of this is manual at the moment
+# Nor does script remove the .ISO once script is ran.
 
+# Questions
+$VMName = Read-Host "Enter the VM name"
+$VMCPUs = Read-Host "Number of CPU cores?"
+$VMMemory = Read-Host "Amount of RAM in MB (e.g., 8192 = 8GB)"
+$VMDisk = Read-Host "Disk size in GB (32 min)"
+$VMCPUs = [int]$VMCPUs
+$VMMemory = [int]$VMMemory
+$VMDiskSizeMB = [int]$VMDisk * 1024
+$VMPath = "$env:USERPROFILE\VirtualBox VMs\$VMName"
+if (-not (Test-Path $VMPath)) {
+    New-Item -ItemType Directory -Path $VMPath | Out-Null
+}
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 # Grab .ISO
@@ -26,10 +33,9 @@ if (-not $ISOPath) {
 # VBoxManage [This is the default path of vboxmanage when Virtualbox is installed, change if you need to.]
 $VBoxManage = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 & "$VBoxManage" createvm --name "$VMName" --ostype Windows10_64 --register
-& "$VBoxManage" modifyvm "$VMName" --memory $VMMemory --vram 128 --cpus $VMCPUs --nic1 nat
-& "$VBoxManage" createhd --filename "$vmPath\$VMName.vdi" --size ($VMDisk)
+& "$VBoxManage" modifyvm "$VMName" --memory $VMMemory --cpus $VMCPUs --vram 128 --ioapic on --boot1 dvd --nic1 nat
+& "$VBoxManage" createhd --filename "$VMPath\$VMName.vdi" --size $VMDiskSizeMB
 & "$VBoxManage" storagectl "$VMName" --name "SATA Controller" --add sata --controller IntelAhci
 & "$VBoxManage" storageattach "$VMName" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VMPath\$VMName.vdi"
 & "$VBoxManage" storageattach "$VMName" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "$ISOPath"
-& "$VBoxManage" modifyvm "$VMName" --boot1 disk --boot2 dvd --boot3 none --boot4 none
-& "$VBoxManage" startvm "$VMName"
+& "$VBoxManage" startvm "$VMName" --type gui
